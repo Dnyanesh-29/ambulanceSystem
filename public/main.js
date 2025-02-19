bbb64e8226c9b100a559e7d082c65850baf0b040
Dnyanesh Mahendra Bharambe <71424533+Dnyanesh-29@users.noreply.github.com>
2025-02-19 11:15:57 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -83,7 +83,8 @@ window.initMap = initMap;
 window.calculateRoute = calculateRoute;
 
 // WebSocket connection setup
-const socket = new WebSocket('wss://https://ambulancesystem.onrender.com/location-updates');
+const socket = new WebSocket('wss://ambulancesystem.onrender.com/location-updates');
+
 
 setInterval(() => {
     if (navigator.geolocation) {
